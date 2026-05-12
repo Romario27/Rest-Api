2f662eeb31d47d991edc6a8fdb795495cc80a0ec
Romario Ramirez <romario_r_r@hotmail.com>
2026-05-11 19:55:51 -0600

    add files

---
+++ backend.js
@@ -0,0 +1,202 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const { findAvailablePort } = require('./free-port.js')
+
+const accounts = require('./accounts.json')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const {validateUserData} = require('./schemas/newUser')
+const desiredPort = process.env.PORT ?? 3000
+
+const SECRET = 'kungfu_Key'
+
+const app = express()
+app.use(express.json())
+//app.use(cors())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:3000',
+      'http://localhost:8080',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+// CORS PRE-Flight
+// OPTIONS
+
+function verifyToken(req, res, next) {
+  const auth = req.headers.authorization
+
+  if (!auth) return res.status(401).json({ error: 'No token' })
+
+  const token = auth.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, SECRET)
+    req.user = decoded
+    next()
+  } catch {
+    res.status(403).json({ error: 'Token inválido' })
+  }
+}
+
+// Todos los recursos que sean MOVIES se identifica con /movies
+app.get('/movies', verifyToken, (req, res) => { // este metodo se usa tambien para obtener peli x genero
+  
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  
+  
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.get('/movies/title/:title', (req, res) => {
+  const { title } = req.params
+  const movie = movies.find(movie => movie.title === title)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies',verifyToken, (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  // en base de datos
+  const newMovie = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = { // este paso lo que hace es como copiar los parametros, que coinciden, q esta en result en movies[index] 
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.post('/login', (req,res) => {
+  const {username, password} = req.body
+
+  const user= accounts.find(user => user.username === username && user.password === password)
+
+  if (!user){
+    return res.status(401).json({error: 'User Invalid. Please try again!!'})
+  }
+
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    SECRET,
+    { expiresIn: '1h' }
+  )
+
+  res.json({ token })
+
+})
+
+app.post('/newAcc' , (req,res) =>{
+  const result = validateUserData(req.body)
+  
+
+  if (!result.success) {
+    console.log(result.error.message)
+    // 422 Unprocessable Entity
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  console.log("No fail")
+   // en base de datos
+  const newUser = {
+    id: crypto.randomUUID(), // uuid v4
+    ...result.data
+  }
+
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  accounts.push(newUser)
+
+  res.status(201).json(newUser)
+
+})
+
+
+/*findAvailablePort(desiredPort).then(port => {
+  app.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
+})*/
+
+app.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
---
+++ schemas/newUser.js
@@ -0,0 +1,26 @@
+const z = require('zod')
+
+const userSchema = z.object({
+    username: z.string({
+        invalid_type_error: 'Username must be a string.',
+        required_error: 'Username is required.'
+    }),
+    password: z.string(),//.min(4, "Minimum 4 characters")
+        //.regex(/[A-Z]/, "Must contain at least one uppercase letter")
+        //.regex(/[a-z]/, "Must contain at least one lowercase letter")
+        //.regex(/[0-9]/, "Must contain at least one number")
+        //.regex(/[^A-Za-z0-9]/, "Must contain at least one special character"),
+    email: z.string({
+        invalid_type_error: 'Email must be a string.',
+        required_error: 'Email is required.'
+    }).email(),
+    age: z.number().int().min(18).max(99)
+})
+
+function validateUserData (input) {
+    return userSchema.safeParse(input)
+}
+
+module.exports = {
+    validateUserData
+}
