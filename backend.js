76946ed9989989f8a35f79cf780d384c3738de2e
Romario Ramirez <romario_r_r@hotmail.com>
2026-05-12 21:11:08 -0600

    update CORS error

--- backend.js
+++ backend.js
@@ -18,6 +18,7 @@ app.use(express.json())
 app.use(cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
+      'https://appfrontend-yme6.onrender.com'
       'http://localhost:3000',
       'http://localhost:8080',
       'https://movies.com',
